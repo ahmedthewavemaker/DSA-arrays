7cf1b1533a3758b047baa1fbbd8e8f96fdd70586
ahmedthewavemaker <72584721+ahmedthewavemaker@users.noreply.github.com>
2021-05-31 12:20:22 -0400

    Update filter-array.js

--- filter-array.js
+++ filter-array.js
@@ -1,1 +1,12 @@
 
+function arrayFilter(arr, filt){
+  let output = [];
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] > filt){
+      output.push(arr[i])
+    }
+  }
+  return output
+}
+
+arrayFilter([1,6,7,8,], 5)
